960fec5b9893878b26c58c5eb59becdc96af5719
galmeriol <imzafer@yahoo.com>
2015-08-03 15:39:01 +0300

    ex-2 added

--- ex1/computeCost.m
+++ ex1/computeCost.m
@@ -7,16 +7,8 @@ function J = computeCost(X, y, theta)
 m = length(y); % number of training examples
  
 J = 0;
-sum = 0;
-for addition_step = 1:m
-    sum_in = 0;
-    sum_in = sum_in + theta(1)*X(addition_step, 1);
-    sum_in = sum_in + theta(2)*X(addition_step, 2);
-    sum_in = sum_in - y(addition_step);
-    sum_in = power(sum_in, 2);
-    sum = sum + sum_in;
-end
 
-J = (1/(2*m))*sum;
+h = X*theta;
 
+J = (1/(2*m))*sum((h-y).^2);
 end
